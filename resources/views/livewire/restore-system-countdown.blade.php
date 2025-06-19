90bc107ef564e5ac47362286447d02b7b5b202b1
carly <carly.cheung@solutionforest.net>
2025-06-19 18:16:24 +0800

    fix alert big

--- resources/views/livewire/restore-system-countdown.blade.php
+++ resources/views/livewire/restore-system-countdown.blade.php
@@ -1,5 +1,5 @@
 <div wire:init="updateCountdown" wire:poll.visible wire:poll.1s="updateCountdown">
-    <x-inspirecms::alert color="danger" size="md" type="danger" icon="heroicon-o-exclamation-circle" class="mt-3">
+    <x-inspirecms::alert color="danger" size="md" type="danger" icon="heroicon-o-exclamation-circle" class="mt-3" :assetReady="true">
         <x-slot:message>
             {{ $this->getDisplayMessage() }} 
         </x-slot:message>
